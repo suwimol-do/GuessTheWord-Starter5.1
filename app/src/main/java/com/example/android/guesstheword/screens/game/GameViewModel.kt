c3661967ff3523691656512f9d816ad419cb5373
Suwimol <Suwimol.do@rmuti.ac.th>
2020-08-02 12:50:11 +0700

    Step 1: Add the GameViewModel class 5.2

---
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.android.guesstheword.screens.game
+
+import android.util.Log
+
+class GameViewModel {
+    init {
+        Log.i("GameViewModel", "GameViewModel created!")
+    }
+}
