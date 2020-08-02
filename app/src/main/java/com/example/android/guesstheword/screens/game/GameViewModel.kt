fb2bd62ad8539cac4dda8efa35cf0eb8f990e49b
Suwimol <Suwimol.do@rmuti.ac.th>
2020-08-02 13:27:43 +0700

    Step 2: Override onCleared() and add logging

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,9 +1,14 @@
 package com.example.android.guesstheword.screens.game
 
 import android.util.Log
+import androidx.lifecycle.ViewModel
 
-class GameViewModel {
+class GameViewModel:ViewModel() {
     init {
         Log.i("GameViewModel", "GameViewModel created!")
+    }
+    override fun onCleared() {
+        super.onCleared()
+        Log.i("GameViewModel", "GameViewModel destroyed!")
     }
 }
